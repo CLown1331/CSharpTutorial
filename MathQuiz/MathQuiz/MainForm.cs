f2032d24fbcadb36f3280ef55bcb7602a70285c4
Araf Al-Jami <CLown1331@users.noreply.github.com>
2018-05-01 17:46:52 +0600

    MathQuiz

---
+++ MathQuiz/MathQuiz/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Araf Al-Jami
+ * Date: 5/1/2018
+ * Time: 5:45 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MathQuiz
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
